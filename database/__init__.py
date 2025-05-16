5c91c9ed4385dd413fbd7a476f9bbbf8fbbef3af
whoami847 <mrbeastbangla069@gmail.com>
2025-05-16 12:49:00 +0600

    Create __init__.py

---
+++ database/__init__.py
@@ -0,0 +1,9 @@
+#
+# Copyright (C) 2025 by AnimeLord-Bots@Github, < https://github.com/AnimeLord-Bots >.
+#
+# This file is part of < https://github.com/AnimeLord-Bots/FileStore > project,
+# and is released under the MIT License.
+# Please see < https://github.com/AnimeLord-Bots/FileStore/blob/master/LICENSE >
+#
+# All rights reserved.
+#
